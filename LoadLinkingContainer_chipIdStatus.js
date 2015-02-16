1d9024f8cecc3efa16fb078ddfbca8c718ca5d38
Peter Zumbruch <p.zumbruch@gsi.de>
2015-02-16 13:46:22 +0100

    Squashed commit of the following:
    commit c940b234ef4c0b5d469fa1191fb09adcd8b35c6d
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Mon Feb 16 13:43:07 2015 +0100
    
        finished implementing GUIs
    
    commit 2b3cab23bbf004b586c2151aa46b8f31441ab1d3
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Mon Feb 16 13:20:45 2015 +0100
    
        reset back to "no action" button
    
    commit 157eb3b157592aea771e9fa218763549e16fef31
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Mon Feb 16 13:19:58 2015 +0100
    
        preliminary, non working column/row action buttons
    
    commit d29776aae6b5471766a97160c33248e741a31706
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Fri Feb 13 18:09:43 2015 +0100
    
        optimizing
    
    commit 2692a6c9c1cf2a661e3ca27e11246c6266b0b2bd
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Fri Feb 13 17:24:55 2015 +0100
    
        introducing and bugfixing dynamic main
    
    commit d5d6ebe644556f618dcb31f772012edce61bd624
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Thu Feb 12 17:19:57 2015 +0100
    
        on the way to correct pvs and generic arrays
    
    commit 152e77a91238c3a7f093bda4fad1661b51f7d813
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Wed Feb 11 15:12:21 2015 +0100
    
        adjusting pv names and macros
    
    commit 71525c35c4bced7f8bc78c4bd15cb3b4b44ab980
    Author: Peter Zumbruch <p.zumbruch@gsi.de>
    Date:   Wed Feb 11 14:48:51 2015 +0100
    
        added hex switch to dac views
        added simple streamDebug opi

---
+++ LoadLinkingContainer_chipIdStatus.js
@@ -0,0 +1,71 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+widget.removeAllChildren();
+
+for(var row=-1; row<=0xF; row++){
+	for(var column=-1; column<=0xF; column++){
+		//create linking container
+		var linkingContainer = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");	
+		linkingContainer.setPropertyValue("opi_file", "chipIdStatus.opi");
+		linkingContainer.setPropertyValue("auto_size", true);
+		linkingContainer.setPropertyValue("zoom_to_fit", false);
+		linkingContainer.setPropertyValue("border_style", 0);
+
+
+        if (-1 == column && -1 == row ) // corner
+        {
+			linkingContainer.setPropertyValue("group_name", "corner");
+			linkingContainer.setPropertyValue("visible", false);
+        }
+        if (0xF == column && 0xF == row ) // corner
+        {
+			linkingContainer.setPropertyValue("group_name", "corner");
+			linkingContainer.setPropertyValue("visible", false);
+        }
+        if (0x0 == column && 0x0 == row ) // corner
+        {
+			linkingContainer.setPropertyValue("group_name", "corner");
+			linkingContainer.setPropertyValue("visible", false);
+        }
+		else if ( -1 == column ) // row	
+		{
+			linkingContainer.setPropertyValue("group_name", "rowLabel");
+			linkingContainer.addMacro("chipIdMin", ((row==0)? 1 : row << 4).toString());
+			linkingContainer.addMacro("stepSize", "1");
+			linkingContainer.addMacro("nChips", (row==0|row==0xF)?"15":"16");
+			linkingContainer.addMacro("nColumns", "4");
+		}
+		else if ( -1 == row ) // column	
+		{
+			linkingContainer.setPropertyValue("group_name", "columnLabel");
+			linkingContainer.addMacro("chipIdMin", ((column==0)? 0x10 : column ).toString());
+			linkingContainer.addMacro("stepSize", "16");
+			linkingContainer.addMacro("nChips", (column==0|column==0xF)?"15":"16");
+			linkingContainer.addMacro("nColumns", "4");
+		}
+		else
+		{
+			linkingContainer.setPropertyValue("group_name", "roundStatus");
+		}
+
+		//add macros
+		var number = (column +  row * 16);
+		linkingContainer.addMacro("chipId", number);
+		linkingContainer.addMacro("chipIdHex", ("0x" + number.toString(16).toUpperCase()));
+		linkingContainer.addMacro("chipIdLow", column);
+		linkingContainer.addMacro("chipIdColumnHex", ("0xx" + column.toString(16).toUpperCase()));
+		linkingContainer.addMacro("chipIdHigh", row);
+		linkingContainer.addMacro("chipIdRowHex", ("0x" + row.toString(16).toUpperCase() + "x"));
+
+		//add linking container to widget
+		widget.addChildToBottom(linkingContainer);	
+	}
+}
+
+var gridLayout = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.gridLayout");
+gridLayout.setPropertyValue("number_of_columns", (16 + 1));	
+gridLayout.setPropertyValue("grid_gap", 1);	
+widget.addChildToBottom(gridLayout);
+widget.performAutosize();
+
+
